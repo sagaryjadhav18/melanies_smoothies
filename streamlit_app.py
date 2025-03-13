9732d60ca576191c04ff9cff27f0f480ccd149a5
sagaryjadhav18 <sagaryjadhav18@gmail.com>
2025-03-13 19:31:14 +0530

    Update streamlit_app.py
    Adding pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
+import pandas as pd
 
 
 # Write directly to the app
@@ -12,9 +13,13 @@ st.write("Choose the fruits you want in your custom smoothie!")
 #col function is used to only display fruit_name in the streamlit app instead of both columns
 cnx = st.connection('snowflake')
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
+#convert the snowpark dataframe to pandas dataframe so we can use the LOC function
+pd_df = my_dataframe.to_pandas()
+st.dataframe(pd_df)
+st.stop()
 name_on_order = st.text_input('Name on Smoothie:')
 st.write('Name on smoothie will be:',name_on_order)
 
@@ -31,6 +36,8 @@ if ingredients_list:
  ingredients_string = ''
  for fruit_chosen in ingredients_list:
      ingredients_string += fruit_chosen + ' '
+     search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+     st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
      st.subheader(fruit_chosen + ' Nutrition Information')
      smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+fruit_chosen)
      #st.text(smoothiefroot_response.json()) #Make Response 200 calls from api readable in json format
